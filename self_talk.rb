9531c7cec7dce1bf8a7072732697bf0abe348c57
MELISSA GUION <M.GUION@GMAIL.COM>
2016-05-30 16:03:30 -0500

    update to method to convert portion of input string and display on change_it.erb

--- self_talk.rb
+++ self_talk.rb
@@ -1,15 +1,22 @@
-def affirmation
-  puts "Type your negative self-talk here:"
-  affirmation = gets.chomp
-  if affirmation.match(/hate/i)
-  affirmation.gsub! 'hate', 'love'
-  elsif affirmation.match(/ugly/i)
-  affirmation.gsub! 'ugly', 'beautiful'
-  elsif affirmation.match(/fat/i)
-  affirmation.gsub! 'fat', 'amazing'
-  else affirmation.match(/terrible/i)
-  affirmation.gsub! 'terrible', 'wonderful'
-  end
+require 'sinatra'
+require 'pry'
+require 'erb'
+
+get '/' do
+  erb :form
 end
 
-affirmation
+post '/' do
+  affirmation = params[:affirmation]
+    if affirmation.match(/hate/i)
+    affirmation.gsub! 'hate', 'love'
+    elsif affirmation.match(/ugly/i)
+    affirmation.gsub! 'ugly', 'beautiful'
+    elsif affirmation.match(/fat/i)
+    affirmation.gsub! 'fat', 'amazing'
+    else affirmation.match(/terrible/i)
+    affirmation.gsub! 'terrible', 'wonderful'
+    change = "#{affirmation}"
+    erb :change_it, locals: { change: change }
+  end
+end
